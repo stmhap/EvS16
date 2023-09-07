3702a058a5bcba2f0024c354d66906f76799e154
stmhap <141840169+stmhap@users.noreply.github.com>
2023-09-08 02:57:11 +0530

    changed d_ff from 128 to 256

--- transformer_model.py
+++ transformer_model.py
@@ -219,7 +219,7 @@ class Transformer(nn.Module):
         # (batch, -seq_len, -vocab_size)
         return self.projection_layer(x)
     
-def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int=512, N: int=6, h: int=8, dropout: float=0.1, d_ff: int=128) -> Transformer:
+def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int=512, N: int=6, h: int=8, dropout: float=0.1, d_ff: int=256) -> Transformer:
         
         # Create the embedding: layers
         src_embed = InputEmbeddings(d_model, src_vocab_size)
